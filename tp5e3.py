b3adf349a420948065c0e2c0776fc18de9cf6667
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-24 12:36:23 -0300

    ya esta la función, le voy a agregar el ingreso entero para que tome un valor ingresado por el usuario

--- tp5e3.py
+++ tp5e3.py
@@ -6,11 +6,34 @@
 
 # Reemplazar por las funciones del ejercicio
 
+def tribonaci(numero_de_la_sucecion):
+    ultimo_valor = 1
+    valor_del_medio = 1
+    primer_valor = 1
+    
+    tribonaci = 1
+    cuenta = 0
+    
+    
+    
+    while cuenta < 9:
+        cuenta += 1
+        
+        ultimo_valor = valor_del_medio
+        valor_del_medio = primer_valor
+        primer_valor = tribonaci
+        
+        lista_de_prueba = [ultimo_valor, valor_del_medio, primer_valor]
+        
+        tribonaci = ultimo_valor + valor_del_medio + primer_valor
+        print(lista_de_prueba)
+    return tribonaci
+
 
 def prueba():
-    """Toda la interacción con el usuario va acá"""
-    pass
-
+    
+    numero = tribonaci()
+    print(f" el enesimo valor en la sucecion de tribonaci es '{numero}'")
+    
 if __name__ == "__main__":
     prueba()
-
