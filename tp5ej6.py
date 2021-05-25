a6e0cb5ca4b814b0bda0ed23cd58712ffe7e578f
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-25 16:33:24 -0300

    6 incompleto. ni idea de que hacer

---
+++ tp5ej6.py
@@ -0,0 +1,61 @@
+################
+# Ivo Spezzacatena - @ivospezza
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+    
+def CadenaDeParentesis(cadena):
+    
+    prueba = list(cadena)
+    
+    for i in prueba:
+        
+        if i != "[" and i != "]" and i != "}" and i != "{" and i!= "(" and i != ")":
+            raise NameError('no ingresaste parentesis')
+        
+  
+    parentesis ="".join(prueba)
+    return parentesis
+
+def Balanceo(cadena_de_parentesis):
+
+    testeo = list(cadena_de_parentesis)
+  
+    valor=0
+    
+    for i in testeo:
+        valor += 1
+        
+    valor -= 1
+    
+    if testeo[0] == "]" or testeo[0] == "}" or testeo[0] == ")":
+        return "cadena no balanceada"
+    
+    elif testeo[valor] == "[" or testeo[valor] == "{" or testeo[valor] == "(":
+        return "cadena no balanceada"
+    
+    
+    elif testeo [1] == "[":
+        for h in testeo:
+            if h == "]":
+                return "cadena balanceada"
+            
+            else:
+                return "cadena no balanceada"
+    
+
+def prueba():
+    
+    cadena = input("increse SOLO paretesis ")
+    cadena = CadenaDeParentesis(cadena)
+    
+    casa = Balanceo(cadena)
+    print(f"'{cadena}' {casa}")
+  
+    
+    
+if __name__ == "__main__":
+    prueba()
+
