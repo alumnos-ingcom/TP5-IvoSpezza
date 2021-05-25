91649d2cabfb796f93323f4e15c75876811b9740
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-25 11:25:42 -0300

    ejercicio 5, todabia no combierte el resultado a plabra

--- tp5ej5.py
+++ tp5ej5.py
@@ -7,10 +7,33 @@
 # Reemplazar por las funciones del ejercicio
     
     
+def palabra_inador(palabra): 
+    
+    palabra_separada = list(palabra)
+    
+    palabra_separada_cambiada = []
+    
+    for letra in palabra_separada:
+        
+        if letra.islower():
+            
+            letra_cambiada = letra.upper()
+            
+            palabra_separada_cambiada.append(letra_cambiada)
+            
+        elif letra.isupper():
+            
+            letra_cambiada = letra.lower()
+            
+            palabra_separada_cambiada.append(letra_cambiada)
+    
+    
+    print(palabra_separada_cambiada)
     
 def prueba():
-    """Toda la interacción con el usuario va acá"""
-    pass
+    
+    palabra_inador(input("ingrese una palabra "))
+    
 
 if __name__ == "__main__":
     prueba()
