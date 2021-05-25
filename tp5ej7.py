140e234284918cfd5fa61a0e8cc84dcecead1f33
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-25 16:45:02 -0300

    ejercicio 7. mucho mas simple para mi que el 6 jajaj

---
+++ tp5ej7.py
@@ -0,0 +1,32 @@
+################
+# Ivo Spezzacatena - @ivospezza
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+from ingreso_float import ingreso_float
+    
+def Distancia(valor_a, valor_b):
+    
+    if valor_a > valor_b:
+        valor_b = valor_b * -1
+        
+    elif valor_b > valor_a:
+        valor_a = valor_a * -1
+        
+    distancia = valor_a + valor_b
+    
+    return distancia
+    
+def prueba():
+    valor_a = ingreso_float("ingrese el primer valor")
+    valor_b = ingreso_float("ingrese el segundo valor")
+    
+    distancia_entre = Distancia(valor_a, valor_b)
+    
+    print(f"la distancia entre '{valor_a}' y '{valor_b}' es '{distancia_entre}'")
+    
+if __name__ == "__main__":
+    prueba()
+
