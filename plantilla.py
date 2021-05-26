72ef3ad04befa649449fd3105b0174a2d6c1b880
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-26 11:24:45 -0300

    ej8 listo pero aun no implemente que funcione con numeros

--- plantilla.py
+++ plantilla.py
@@ -6,12 +6,107 @@
 
 # Reemplazar por las funciones del ejercicio
     
+from ingreso_entero import ingreso_entero
+
     
+def Codificador(texto, cantidad):
+    
+    texto_ingresado = list(texto)
+    
+    armando_texto = []
+    
+    
+    
+    if cantidad > 26:
+        
+        while cantidad > 26:
+            cantidad -= 26
+            
+    minus = list("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+    mayus = list("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    
+    pocicion = 0
+    
+    pocicion_final = 0
+    
+    for letra in texto_ingresado:
+        
+        if letra.islower():
+           
+            pocicion = minus.index(letra)
+            pocicion_final = pocicion + cantidad 
+            armando_texto.append(minus[pocicion_final])
+            
+        elif letra.isupper():
+            pocicion = mayus.index(letra)
+            pocicion_final = pocicion + cantidad 
+            armando_texto.append(mayus[pocicion_final])
+            
+        elif letra == " ":
+            armando_texto.append(" ")
+            
+    palabra_codificada = "".join(armando_texto)
+    
+    return palabra_codificada
+            
+
+    
+def Decodificador(texto,cantidad):
+    
+    texto_ingresado = list(texto)
+    
+    armando_texto = []
+    
+    
+    
+    if cantidad > 26:
+        
+        while cantidad > 26:
+            cantidad -= 26
+            
+    minus = list("zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba")
+    mayus = list("ZYXWVUTSRQPONMLKJIHGFEDCBAZYXWVUTSRQPONMLKJIHGFEDCBA")
+    
+    pocicion = 0
+    
+    pocicion_final = 0
+    
+    for letra in texto_ingresado:
+        
+        if letra.islower():
+           
+            pocicion = minus.index(letra)
+            pocicion_final = pocicion + cantidad 
+            armando_texto.append(minus[pocicion_final])
+            
+        elif letra.isupper():
+            pocicion = mayus.index(letra)
+            pocicion_final = pocicion + cantidad 
+            armando_texto.append(mayus[pocicion_final])
+            
+        elif letra == " ":
+            armando_texto.append(" ")
+            
+    palabra_codificada = "".join(armando_texto)
+    
+    return palabra_codificada
+
+
     
 def prueba():
-    """Toda la interacción con el usuario va acá"""
-    pass
-
+   
+    texto = input("ingrese un texto ")
+       
+    nivel_codificado = ingreso_entero("nivel de codificado") 
+    
+    texto_codeado = Codificador(texto, nivel_codificado)
+    
+    print(texto_codeado)
+    
+    texto_otra_vez = Decodificador(texto_codeado, nivel_codificado)
+    
+    print(texto_otra_vez)
+    
 if __name__ == "__main__":
     prueba()
 
