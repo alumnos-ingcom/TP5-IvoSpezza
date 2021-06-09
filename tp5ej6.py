96100ef97babaeae677939823da630ba5d616345
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-06-09 14:22:48 -0300

    repensando el 6

--- tp5ej6.py
+++ tp5ej6.py
@@ -6,53 +6,14 @@
 
 # Reemplazar por las funciones del ejercicio
     
-def CadenaDeParentesis(cadena):
-    
-    prueba = list(cadena)
-    
-    for i in prueba:
-        
-        if i != "[" and i != "]" and i != "}" and i != "{" and i!= "(" and i != ")":
-            raise NameError('no ingresaste parentesis')
-        
-  
-    parentesis ="".join(prueba)
-    return parentesis
 
 def Balanceo(cadena_de_parentesis):
 
-    testeo = list(cadena_de_parentesis)
-  
-    valor=0
     
-    for i in testeo:
-        valor += 1
-        
-    valor -= 1
-    
-    if testeo[0] == "]" or testeo[0] == "}" or testeo[0] == ")":
-        return "cadena no balanceada"
-    
-    elif testeo[valor] == "[" or testeo[valor] == "{" or testeo[valor] == "(":
-        return "cadena no balanceada"
-    
-    conteo = 0
-    
-    while conteo < valor:
-        if testeo[conteo] == testeo[conteo + 1]:
-            
-        conteo += 1
     
 
 def prueba():
-    
-    cadena = input("increse SOLO paretesis ")
-    cadena = CadenaDeParentesis(cadena)
-    
-    casa = Balanceo(cadena)
-    print(f"'{cadena}' {casa}")
-  
-    
+    pass
     
 if __name__ == "__main__":
     prueba()
